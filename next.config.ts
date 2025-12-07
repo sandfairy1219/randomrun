6e43ff9d98b1060af20fd045bec8244c868a5e84
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2025-12-08 04:21:40 +0900

    Refactor next.config.ts to use inline config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,1 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    formats: ["img/avif", "img/webp"],
-  },
-};
-
-export default nextConfig;
+import type { NextConfig } from "next"; const nextConfig: NextConfig = { /* config options here */ }; export default nextConfig;
