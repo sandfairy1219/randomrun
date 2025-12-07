65647a6a974c7678f6a70486d5e8712155b8262d
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2025-12-08 04:20:28 +0900

    Change image formats from 'image' to 'img'

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["img/avif", "img/webp"],
   },
 };
 
